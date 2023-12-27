db9b9cf73b7118741cf62442163a2ce2f34aa0a8
Oleh Mushka <olegamysk@gmail.com>
2023-12-27 15:31:19 +0200

    initialized package

---
+++ postgresfx/module.go
@@ -0,0 +1,45 @@
+package postgresfx
+
+import (
+	"context"
+
+	"github.com/ecumenos/fxecumenos/postgresfx/postgres"
+	"github.com/jackc/pgx/v4"
+	"go.uber.org/fx"
+)
+
+type Config struct {
+	URL            string `json:"url"`
+	MigrationsPath string `json:"migrationsPath"`
+}
+
+var Module = fx.Options(
+	fx.Provide(func(lc fx.Lifecycle, cfg *Config) (Driver, error) {
+		driver, err := postgres.New(context.Background(), cfg.URL)
+		if err != nil {
+			return nil, err
+		}
+		lc.Append(fx.Hook{
+			OnStart: func(ctx context.Context) error {
+				return driver.Ping(ctx)
+			},
+			OnStop: func(context.Context) error {
+				driver.Close()
+				return nil
+			},
+		})
+
+		return driver, nil
+	}),
+)
+
+//go:generate mockery --name=Driver
+
+type Driver interface {
+	Ping(ctx context.Context) error
+	Close()
+	CountRows(ctx context.Context, query string, args ...interface{}) (int, error)
+	ExecuteQuery(ctx context.Context, query string, args ...interface{}) error
+	QueryRow(ctx context.Context, query string, args ...interface{}) (pgx.Row, error)
+	QueryRows(ctx context.Context, query string, args ...interface{}) (pgx.Rows, error)
+}
